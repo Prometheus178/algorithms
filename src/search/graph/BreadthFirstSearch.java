2ca671276b9848004c1276e805c41f65a8a392df
sergey <sergeipopof178@gmail.com>
2022-06-30 09:56:36 +0300

    BFS

--- src/search/graph/BreadthFirstSearch.java
+++ src/search/graph/BreadthFirstSearch.java
@@ -1,17 +1,33 @@
 package search.graph;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Hashtable;
+import java.util.*;
 
 /**
  * Author: Sergey.
  */
 public class BreadthFirstSearch {
 
-    public static String find(Hashtable<String, String[]> graph) {
-        Deque<String> deque = new ArrayDeque<>();
+    public static boolean findNameEndedAtM(Hashtable<String, String[]> graph) {
+        Deque<String> searchDeque = new ArrayDeque<>(Arrays.asList(graph.get("you")));
+        ArrayList<String> checked = new ArrayList<>();
+        while (!searchDeque.isEmpty()) {
+            String person = searchDeque.poll();
+            if (person.endsWith("m")){
+                return true;
+            }else {
+                if (!checked.contains(person)){
+                    String[] strings = graph.get(person);
+                    if (strings.length != 0){
+                        List<String> friends= Arrays.asList(strings);
+                        if (!friends.isEmpty()){
+                            searchDeque.addAll(friends);
+                        }
+                    }
 
-        return "";
+                }
+                checked.add(person);
+            }
+        }
+        return false;
     }
 }
--- src/search/graph/Demo.java
+++ src/search/graph/Demo.java
@@ -12,11 +12,14 @@ public class Demo {
         graph.put("you", new String[]{"alice", "bob", "claire"});
         graph.put("bob", new String[]{"anuj", "peggy"});
         graph.put("alice", new String[]{"peggy"});
-        graph.put("claire", new String[]{"thom", "jonny"});
+        graph.put("claire", new String[]{"jonny"});
         graph.put("anuj", new String[]{});
         graph.put("peggy", new String[]{});
+        graph.put("jonny", new String[]{"thom"});
         graph.put("thom", new String[]{});
-        graph.put("jonny", new String[]{});
+
+        boolean nameEndedAtM = BreadthFirstSearch.findNameEndedAtM(graph);
+        System.out.println(nameEndedAtM);
 
     }
 }
